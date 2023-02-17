6d241c27f1fb2f0ccab424929a5f7b2c9badc582
shiba6v <shiba6v@gmail.com>
2023-02-17 23:46:19 +0900

    license

--- cmd/refillstruct/fill.go
+++ cmd/refillstruct/fill.go
@@ -1,3 +1,9 @@
+// Copyright (c) 2023 shiba6v All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Some codes are copied from fillstruct from David R. Jenni.
+// Here is the original copyright.
 // Copyright (c) 2018 David R. Jenni. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
